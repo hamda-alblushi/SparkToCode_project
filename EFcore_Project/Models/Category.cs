b0622ab2b9066fd6a1786d6f10d53a5e0a3f0575
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-07-29 17:35:45 +0400

    Add order details

---
+++ EFcore_Project/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFcore_Project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int categoryId { get; set; }
+        public string categoryName { get; set; }
+
+
+        [InverseProperty("Category")]
+        public List<Product> products { get; set; }
+    }
+}
