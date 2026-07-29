842df22c149aaa0a5e8e6b9f35253ecb68d0ae73
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-07-30 01:46:55 +0400

    built all classes with relationships

---
+++ EFcore_Project/ProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using EFcore_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFcore_Project
+{
+    public class ProjectContext : DbContext
+    {
+        public DbSet<User> users { get; set; }
+        public DbSet<Review> reviews { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderProducts> orderProducts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=.\\SQLEXPRESS;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}
