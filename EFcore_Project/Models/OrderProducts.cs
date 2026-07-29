dad5bd763d1160af7c4e08cdf1f4902a997e036b
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-07-29 17:36:15 +0400

    Add product details

---
+++ EFcore_Project/Models/OrderProducts.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFcore_Project.Models
+{
+    [PrimaryKey("orderId", "productId")]
+    public class OrderProducts
+    {
+        [ForeignKey("orderProducts")]
+        public int orderId { get; set; }
+
+
+        [ForeignKey("productOrders")]
+        public int productId { get; set; }
+
+
+        public int quantity { get; set; }
+    }
+}
