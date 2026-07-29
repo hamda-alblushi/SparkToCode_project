2beb101934973a0c10cccd3ccf9fbab85d71f3cf
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-07-30 01:48:40 +0400

    Add order details

---
+++ EFcore_Project/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFcore_Project.Models
+{
+    public class Order
+    {
+        [Key]
+        public int orderId { get; set; }
+        public string orderDate { get; set; }
+
+
+        [ForeignKey("user")]
+        public int userId { get; set; }
+        public User user { get; set; }
+
+
+        [InverseProperty("order")]
+        public Review review { get; set; }
+
+
+        public List<OrderProducts> products { get; set; }
+    }
+}
