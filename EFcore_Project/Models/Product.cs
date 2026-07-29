96039b664e63f212d7c0305080a1c0c8a9e6f018
hamda essa alblushi <hamdaalb2005@gmail.com>
2026-07-30 01:47:52 +0400

    Add product details

---
+++ EFcore_Project/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFcore_Project.Models
+{
+    public class Product
+    {
+        [Key]
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public double productPrice { get; set; }
+
+
+        [ForeignKey("category")]
+        public int categoryId { get; set; }
+        public Category category { get; set; }
+
+
+        public List<OrderProducts> orders { get; set; }
+    }
+}
